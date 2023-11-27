bafdc7992f6b8347bd4a1aaa9f28488365d090a2
yuxi <1305711389@qq.com>
2023-11-27 11:14:36 +0800

    add basic modules

---
+++ im-live-msg/im-live-msg-provider/src/main/java/com/example/msg/ImLiveMsgProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImLiveMsgProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImLiveMsgProviderApplication.class, args);
+    }
+
+}
