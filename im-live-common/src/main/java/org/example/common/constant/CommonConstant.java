267543c7a05483771d1fa53e34c7f56e18197729
yuxi <1305711389@qq.com>
2023-11-28 10:44:43 +0800

    添加nacos基础配置

---
+++ im-live-common/src/main/java/org/example/common/constant/CommonConstant.java
@@ -0,0 +1,22 @@
+package org.example.common.constant;
+
+
+/**
+ * @author tom
+ */
+public interface CommonConstant {
+
+
+
+
+    /** {@code 500 Server Error} (HTTP/1.0 - RFC 1945) */
+    public static final Integer SC_INTERNAL_SERVER_ERROR_500 = 500;
+    /** {@code 200 OK} (HTTP/1.0 - RFC 1945) */
+    public static final Integer SC_OK_200 = 200;
+
+    /**访问权限认证未通过 510*/
+    public static final Integer SC_NO_AUTH=510;
+
+
+
+}
