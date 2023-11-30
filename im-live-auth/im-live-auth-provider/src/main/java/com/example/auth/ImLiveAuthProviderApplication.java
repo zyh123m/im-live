578ef2a63ebd7b88f8ec6465d1b4e6bbc04609a8
yuxi <1305711389@qq.com>
2023-11-30 09:34:26 +0800

    添加auth 的基础模块

---
+++ im-live-auth/im-live-auth-provider/src/main/java/com/example/auth/ImLiveAuthProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImLiveAuthProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImLiveAuthProviderApplication.class, args);
+    }
+
+}
